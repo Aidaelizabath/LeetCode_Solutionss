3c899f123f52728fc147014c1ffd9f8f19505a7c
Aida Elizabath Varghese <aidaelizabathvarghese2003@gmail.com>
2026-06-11 20:20:45 +0530

    Time: 0 ms (100%), Space: 12.7 MB (18.22%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        h=0
+        citations.sort(reverse=True)
+        for i in range(len(citations)):
+            if citations[i]>=i+1:
+                h = i+1
+            else:
+                break
+        return h
+
+        
