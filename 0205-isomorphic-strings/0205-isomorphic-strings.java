f9b484357c42e0a388acd8fb1058bb2d6bee4954
Aida Elizabath Varghese <aidaelizabathvarghese2003@gmail.com>
2026-07-31 11:58:52 +0530

    Time: 12 ms (58.03%), Space: 43.6 MB (94.25%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character>map = new HashMap<>();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            char ch1 = t.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(ch1)){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(ch1)){
+            
+                     return false;
+                }
+                else{
+                    map.put(ch,ch1);
+                }
+                }
+            }
+        return true;
+        
+    }
+}
